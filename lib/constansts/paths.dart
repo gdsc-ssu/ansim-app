e7eab0b23f6be408f0b1e12c6628b1dbc51238ea
Seoneui Jee <ellieella2031@gmail.com>
2026-02-23 16:09:13 +0900

    feat: Define application route paths

---
+++ lib/constansts/paths.dart
@@ -0,0 +1,10 @@
+abstract class Paths {
+  Paths._();
+
+  static const String splash = '/splash';
+  static const String login = '/login';
+
+  static const String home = '/home';
+  static const String myPage = '/myPage';
+
+}
