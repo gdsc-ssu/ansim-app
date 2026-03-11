ec9af25ecd4ab318008709cfef99cb07aa6f6de2
Seoneui Jee <ellieella2031@gmail.com>
2026-03-11 14:47:35 +0900

    Feat: Configure Google Maps API key for iOS (Native)

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
@@ -7,6 +8,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCEaCkUogzD5P-m3qynv0HSKW3nSUJnpvY")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
