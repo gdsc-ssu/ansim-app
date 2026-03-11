f2390d59827a56297388da5380e548d30a9cc579
Seoneui Jee <ellieella2031@gmail.com>
2026-03-11 22:59:31 +0900

    feat(ios): Load Google Maps API key from .env file

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,26 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyCEaCkUogzD5P-m3qynv0HSKW3nSUJnpvY")
+    var mapsApiKey = ""
+    if let path = Bundle.main.path(forResource: ".env", ofType: nil) {
+        do {
+            let envString = try String(contentsOfFile: path, encoding: .utf8)
+            let lines = envString.components(separatedBy: .newlines)
+            for line in lines {
+                if line.starts(with: "GOOGLE_MAPS_API_KEY=") {
+                    mapsApiKey = line.replacingOccurrences(of: "GOOGLE_MAPS_API_KEY=", with: "").replacingOccurrences(of: "\"", with: "").trimmingCharacters(in: .whitespaces)
+                    break
+                }
+            }
+        } catch {
+            print("Failed to load .env file")
+        }
+    }
+    
+    if !mapsApiKey.isEmpty {
+        GMSServices.provideAPIKey(mapsApiKey)
+    }
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
