89a2fcc04c7318ea3c737fb83b048d25fca91232
Seoneui Jee <ellieella2031@gmail.com>
2026-03-13 18:55:36 +0900

    chore: Configure app initialization for environment variables and DI

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
+import 'package:ansim_app/data/di/get_it_locator.dart';
 import 'package:ansim_app/route/routes.dart';
 import 'package:ansim_app/screens/map/map_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // 환경변수 로드
+  await dotenv.load(fileName: ".env");
+  
+  // 의존성 주입 설정
+  setupServiceLocator();
+
   runApp(
       MultiProvider(
         providers: [
