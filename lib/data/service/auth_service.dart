619c6b446fd5a34c50417482daf9da44a3e8638b
Seoneui Jee <ellieella2031@gmail.com>
2026-03-13 18:55:35 +0900

    feat: Implement Google access token acquisition in AuthService

--- lib/data/service/auth_service.dart
+++ lib/data/service/auth_service.dart
@@ -1,6 +1,8 @@
 import 'dart:developer';
 
+import 'package:ansim_app/constansts/apis.dart';
 import 'package:ansim_app/data/di/api_client.dart';
+import 'package:ansim_app/data/dto/response/token_response.dart';
 import 'package:ansim_app/data/repository/local/secure_storage_repository.dart';
 import 'package:dio/dio.dart';
 
@@ -9,44 +11,39 @@ class AuthService {
   final SecureStorageRepository _secureStorage = SecureStorageRepository();
 
   /// POST google 서버에서 받은 accessToken으로 서비스 accessToken 발급
- // TODO : 서버 스웨그 보고 맞춰서 수정
-//   Future<TokenResponse> getGoogleAccessToken(
-//       String authCode, String name) async {
-//     try {
-//       final response = await _apiClient.dio.post(
-//         options: Options(
-//           extra: {'skipAuthToken': true}, //토큰 해제
-//         ),
-//         Apis.getGoogleAccessToken,
-//         queryParameters: {
-//           'accessToken': authCode,
-//         },
-//       );
-//       if (response.statusCode == 200) {
-//         // BaseResponse
-//         final baseResponse = BaseResponse<TokenResponse>.fromJson(
-//           response.data as Map<String, dynamic>,
-//               (contentJson) =>
-//               TokenResponse.fromJson(contentJson as Map<String, dynamic>),
-//         );
-//
-// // AccessToken 저장 로직 수정
-//         final tokenResponse = baseResponse.content;
-//
-//         if (tokenResponse != null) {
-//           await _secureStorage.saveAccessToken(tokenResponse.accessToken);
-//           await _secureStorage.saveRefreshToken(tokenResponse.refreshToken);
-//
-//           log("accessToken ${tokenResponse.accessToken} refreshtoken ${tokenResponse.refreshToken}");
-//         }
-//
-//         return tokenResponse;
-//       } else {
-//         throw Exception(
-//             'Failed to send verification code ${response.data.toString()}');
-//       }
-//     } on DioException catch (e) {
-//       throw Exception('Error: ${e.response?.data.toString() ?? e.message}');
-//     }
-//   }
+
+  Future<TokenResponse> getGoogleAccessToken(String authCode, String name) async {
+    try {
+      final response = await _apiClient.dio.post(
+        Apis.getGoogleAccessToken,
+        queryParameters: {
+          'idToken': authCode,
+        },
+        options: Options(
+          extra: {'skipAuthToken': true}, // 인증 토큰 제외 설정
+        ),
+      );
+
+      if (response.statusCode == 200) {
+        // JSON 데이터를 TokenResponse 객체로 변환
+        final tokenResponse = TokenResponse.fromJson(response.data as Map<String, dynamic>);
+
+        // SecureStorage에 저장
+        await _secureStorage.saveAccessToken(tokenResponse.accessToken);
+        await _secureStorage.saveRefreshToken(tokenResponse.refreshToken);
+
+        log("accessToken ${tokenResponse.accessToken} refreshtoken ${tokenResponse.refreshToken}");
+        return tokenResponse;
+      } else {
+        throw Exception('Failed to send verification code: ${response.data.toString()}');
+      }
+    } on DioException catch (e) {
+      // Dio 관련 에러 처리
+      throw Exception('Error: ${e.response?.data.toString() ?? e.message}');
+    } catch (e) {
+      // 기타 런타임 에러 처리
+      throw Exception('Unknown error: $e');
+    }
+  }
 }
+
