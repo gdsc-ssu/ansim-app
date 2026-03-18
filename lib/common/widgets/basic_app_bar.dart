ab9c3e161810a8414bc8e84cf9d55e75ead52a64
Seoneui Jee <ellieella2031@gmail.com>
2026-03-19 00:28:17 +0900

    feat: 스크롤 화면에서 appbar 배경 오류 수정

--- lib/common/widgets/basic_app_bar.dart
+++ lib/common/widgets/basic_app_bar.dart
@@ -18,6 +18,8 @@ class BasicAppBar extends StatelessWidget implements PreferredSizeWidget {
   Widget build(BuildContext context) {
     return AppBar(
       backgroundColor: Colors.white,
+      surfaceTintColor: Colors.transparent,
+      scrolledUnderElevation: 0,
       elevation: 0,
       centerTitle: true,
       leading: showBackButton
